b649aa8a79c38e2c2c5050ecd09d1c8317e2f88f
Eden Aristo Tingkir <eden.aristo@gmail.com>
2024-10-14 23:31:50 +1100

    added the particle class

--- src/Particle.java
+++ src/Particle.java
@@ -1,2 +1,27 @@
-public class Particle {
+import bagel.Font;
+import bagel.Image;
+import bagel.Input;
+import bagel.Keys;
+
+import java.util.Properties;
+
+public class Particle extends GameObject {
+
+    private duration;
+
+    public Particle(int x, int y, Properties props) {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void adjustToInputMovement {
+
+    }
 }
